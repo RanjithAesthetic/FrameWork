4872ed21378c770e07bb8b02da0600e954678c2a
Ranjithasthetic <ranjithraghul1@gmail.com>
2021-12-31 17:12:03 +0530

    Username Functionality

---
+++ src/org/test/Facebook.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Facebook {
+
+	public void username() {
+System.out.println("User name");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/org/test/Sam.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class Sam {
+	private static void username() {
+		System.out.println("Username");
+	}
+	public static void main(String[] args) {
+		username();
+	}
+
+}
